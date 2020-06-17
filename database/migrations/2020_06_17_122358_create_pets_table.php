395757ff9737a9af66438238e7fa417a9e408fce
iarafat <miyasinarafat@gmail.com>
2020-06-17 18:33:03 +0600

    added pet table, model, controller

---
+++ database/migrations/2020_06_17_122358_create_pets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('name');
+            $table->double('weight')->nullable();
+            $table->enum('pet_category', ['CAT', 'DOG', 'RABBIT', 'STINGRAY']);
+            $table->enum('status', ['AVAILABLE', 'CHECKEDOUT']);
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pets');
+    }
+}
